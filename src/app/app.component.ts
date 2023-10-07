050fe592b8ce15998f1cff88ea3e82ccb02605b9
DevinAnnunzio <annunzio.da@gmail.com>
2023-10-07 16:32:07 +0900

    Feat: emit string from child to parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basics-assignment-4';
+  eventEmittedFromGame: string = ''
+
+
+  handleEvent(event: string){
+    this.eventEmittedFromGame = event
+  }
 }
--- src/app/game-control/game-control-component/game-control-component.component.ts
+++ src/app/game-control/game-control-component/game-control-component.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control-component',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./game-control-component.component.css']
 })
 export class GameControlComponentComponent {
+  @Output() countEvent = new EventEmitter<string>();
+  
+  intervalId: any
+  numberTracker: number = 0
 
+  startEmitting(event: EventEmitter<string>){
+    console.log("Event has started")
+    console.log(event)
+    console.log('----------------------------------')
+    this.intervalId = setInterval(() => {
+      // Code to execute at each interval
+      this.countEvent.emit(`Event being emitted ${this.numberTracker}`);
+      this.numberTracker++;
+    }, 1000); // 1000 milliseconds (1 second) interval
+  }
+
+  stopEmitting(){
+    clearInterval(this.intervalId);
+  }
 }
